e90cb2c92b72022476d611d07a893badb8695c59
Fawad <fawadhandsy69@gmail.com>
2024-08-12 13:11:45 +0500

    project added

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/insta', (req, res)=>{
+    res.send('fawadahmad02')
+})
+
+app.get('/login', (req, res)=>{
+    res.send('<h1>Please login at the site</h1>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
